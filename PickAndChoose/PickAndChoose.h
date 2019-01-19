28dc8656bcf12e2c00447687af70071a4ac5f5d7
Brent Michalski <Brent.Michalski@gmail.com>
2019-01-19 14:18:02 -0600

    Initial Commit

---
+++ PickAndChoose/PickAndChoose.h
@@ -0,0 +1,19 @@
+//
+//  PickAndChoose.h
+//  PickAndChoose
+//
+//  Created by Brent Michalski on 1/19/19.
+//  Copyright © 2019 Perlguy, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PickAndChoose.
+FOUNDATION_EXPORT double PickAndChooseVersionNumber;
+
+//! Project version string for PickAndChoose.
+FOUNDATION_EXPORT const unsigned char PickAndChooseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PickAndChoose/PublicHeader.h>
+
+
